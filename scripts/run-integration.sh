1031010d12dbcd156031dc46ac25bb29ee7b35ed
Rahul Srivastava <52713783+rahul-k-srvastva@users.noreply.github.com>
2024-09-18 00:51:16 +0530

    Update run-integration.sh

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,6 +1,6 @@
-docker-compose -f ../docker-compose.yml up -d
+docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
 npm run test
-docker-compose -f ../docker-compose.yml down
+docker-compose down
